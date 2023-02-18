334886f105ea520f502b270356e4292724bff206
Andrii Sirko <andriy251101@gmail.com>
2023-02-18 23:10:00 +0200

    Added documentation for Pagination class

--- src/main/java/com/example/finalproject/pagination/Pagination.java
+++ src/main/java/com/example/finalproject/pagination/Pagination.java
@@ -3,7 +3,19 @@ package com.example.finalproject.pagination;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+/**
+ * The pagination class is responsible for changing the starting page in the list
+ */
 public class Pagination {
+    /**
+     * The method gets parameter "next" or "previous" from request. If parameter equals "next" the method increase the starting page by one,
+     * and if the parameter equals "previous" the method decrease the starting page by one.
+     * @param request HttpServletRequest
+     * @param numberOfRecords number of records
+     * @param startPage start page
+     * @param recordsPerPage records per page
+     * @return the page from which pagination begins
+     */
     public static int pagination(HttpServletRequest request, int numberOfRecords, int startPage, int recordsPerPage) {
         int numberOfPages = (int) Math.ceil(numberOfRecords * 1.0 / recordsPerPage);
         if(startPage > numberOfPages && startPage > 1)
