d32e3a5b3e30c1812edbc709df9e6f73406b514a
Andrii Sirko <andriy251101@gmail.com>
2023-02-28 23:22:19 +0200

    Added test for LogOutCommandTest class

--- src/test/java/com/example/finalproject/command/logout/LogOutCommandTest.java
+++ src/test/java/com/example/finalproject/command/logout/LogOutCommandTest.java
@@ -14,6 +14,7 @@ import static org.mockito.Mockito.*;
 
 
 public class LogOutCommandTest {
+    //Delete
     @Test
     public void whenCallDoGetThenServletReturnIndexPage() throws ServletException, IOException, SQLException {
         final UserCardCommand servlet = new UserCardCommand();
@@ -29,6 +30,21 @@ public class LogOutCommandTest {
 
         verify(request, times(1)).getRequestDispatcher(path);
         verify(request, never()).getSession();
+        verify(dispatcher).forward(request, response);
+    }
+
+    @Test
+    public void whenCallLogOutCommandReturnLoginPage() throws ServletException, SQLException, IOException {
+        final LogOutCommand servlet = new LogOutCommand();
+        final HttpServletRequest request = mock(HttpServletRequest.class, RETURNS_DEEP_STUBS);
+        final HttpServletResponse response = mock(HttpServletResponse.class);
+        final RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+
+        String path = "login/login.jsp";
+        when(request.getRequestDispatcher(path)).thenReturn(dispatcher);
+
+        servlet.execute(request, response);
+        verify(request, atLeast(2)).getSession();
         verify(dispatcher).forward(request, response);
     }
 }
